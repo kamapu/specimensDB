7eb9eb34a5e6273f93ebbb4efe3ae57897712a48
miguel <miguel@miguel-OptiPlex-790>
2022-03-06 16:49:27 +0100

    All bugs solved

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,7 @@
 #' @importFrom methods new setOldClass
 #' @importFrom DBI dbGetQuery
 #' @importFrom sf st_coordinates st_nearest_feature st_read
+#' @importClassesFrom specimens specimens
 ## #' @importFrom yamlme render_rmd txt_body write_rmd
 #' @importClassesFrom RPostgreSQL PostgreSQLConnection
 ## #' @importClassesFrom yamlme rmd_doc
--- lab/01-reading-collection.R
+++ lab/01-reading-collection.R
@@ -3,29 +3,16 @@
 # Author: Miguel Alvarez
 ################################################################################
 
-library(RPostgreSQL)
-library(vegtableDB)
 library(dbaccess)
-library(sf)
+library(specimens)
+library(specimensDB)
 
 conn1 <- connect_db2("vegetation_v3", user = "miguel")
 conn2 <- connect_db2("gadm_v3", user = "miguel")
 
-source("R/read_spec.R")
+Spec <- read_spec(db = conn1, adm = conn2, bulk = 2)
 
-Spec <- read_spec(db = conn1, adm = conn2, tax = "swea_dataveg", bunch = 2)
+validObject(Spec)
 
-saveRDS(Spec, "lab/specimens.rds")
-
-# Arguments
-db <- conn1
-adm <- conn2
-bunch = 2
-
-Coll <- st_read(conn1, query = "select * from specimens.collections")
-
-
-
-
-
-
+# Import object for laboratory in specimens
+saveRDS(Spec, "../specimens/lab/specimens.rds")

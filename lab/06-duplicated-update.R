bb8ba812980e61ac805b2de95bdc377121fae105
miguel <miguel@miguel-OptiPlex-790>
2022-03-27 12:30:58 +0200

    Ready to merge

--- lab/06-duplicated-update.R
+++ lab/06-duplicated-update.R
@@ -12,7 +12,7 @@ install_github("kamapu/specimens", "devel")
 # Load libraries
 library(dbaccess)
 library(specimensDB)
-library(vegtableDB)
+## library(vegtableDB)
 
 # Restore database
 DB <- "vegetation_v3"
@@ -27,7 +27,7 @@ conn <- connect_db2(DB, user = "miguel")
 
 # Import bulk
 SP <- read_spec(conn, bulk = 2)
-TAX <- db2taxlist(conn, "ea_splist")
+## TAX <- db2taxlist(conn, "ea_splist")
 
 # Test duplicated in input
 summary(subset(SP, coll_nr %in% SP@collections$coll_nr[1:2]))
@@ -40,6 +40,7 @@ df1 <- data.frame(
     det = rep("M. Alvarez", 3),
     det_date = rep(as.Date("2022-03-24"), 3))
 
+# duplicates with database
 df2 <- data.frame(
     spec_id = c(879, 954),
     taxon_usage_id = rep(209403, 2),
@@ -47,19 +48,28 @@ df2 <- data.frame(
     det = rep("M. Alvarez", 2),
     det_date = rep(as.Date("2011-03-15"), 2))
 
+# Case 1: duplicated inputs
+specimensDB::update_det(conn, df1)
 
+# Case 2: duplicated inputs but using compare
+specimensDB::update_det(conn, df1, compare = TRUE)
 
+# Case 3: duplicated with determinations in database
+specimensDB::update_det(conn, df2)
 
+# Case 4: same as before but solved in advance
+specimensDB::update_det(conn, df2, compare = TRUE)
 
+# Cross-check
+SP <- read_spec(conn, bulk = 2)
+summary(subset(SP, coll_nr %in% SP@collections$coll_nr[1:2]))
 
+# Refesh database
+do_restore(dbname = DB,
+    user = "miguel",
+    filepath = file.path("../../db-dumps/00_dumps", DB),
+    path_psql = "/usr/bin")
 
-
-
-
-
-## object <- SP
-## format = "%d.%m.%Y"
-library(RPostgreSQL)
-db = conn
-df = df2
-
+# Cross-check again
+SP <- read_spec(conn, bulk = 2)
+summary(subset(SP, coll_nr %in% SP@collections$coll_nr[1:2]))

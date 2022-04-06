1a57324efc1e03b55248ca938fdb4b699ccf21a5
Miguel Alvarez <kamapu78@gmail.com>
2022-04-06 23:09:44 +0200

    New function 'description_sheet()'

--- R/imports.R
+++ R/imports.R
@@ -1,12 +1,15 @@
-#' @importFrom methods as new setOldClass
 #' @importFrom DBI dbGetQuery
-#' @importFrom sf st_coordinates st_drop_geometry st_nearest_feature st_read
-#' @importFrom taxlist replace_x dissect_name
-#' @importFrom utils askYesNo
+#' @importFrom leaflet addCircleMarkers addMiniMap addProviderTiles addScaleBar
+#'     leaflet
+#' @importFrom magrittr %>%
+#' @importFrom mapview mapshot
+#' @importFrom methods as new setOldClass
 #' @importFrom rpostgis pgInsert
-#' @importFrom taxlist dissect_name replace_x
-#' @importFrom yamlme render_rmd txt_body write_rmd
+#' @importFrom sf st_coordinates st_drop_geometry st_nearest_feature st_read
 #' @importFrom specimens as_data.frame
+#' @importFrom taxlist dissect_name replace_x
+#' @importFrom utils askYesNo
+#' @importFrom yamlme render_rmd txt_body write_rmd
 #' @importClassesFrom specimens specimens
 #' @importClassesFrom RPostgreSQL PostgreSQLConnection
 #' @importClassesFrom yamlme rmd_doc
